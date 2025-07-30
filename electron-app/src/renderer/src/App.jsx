6b5bcd3eb2e20a7f95468ffa83b7d7571049cdf1
berk <badday182@gmail.com>
2025-07-30 11:19:24 +0300

    add tasks obj and maped it

--- electron-app/src/renderer/src/App.jsx
+++ electron-app/src/renderer/src/App.jsx
@@ -1,14 +1,28 @@
 import TaskCard from './components/taskCard/TaskCard.jsx'
 // import './components/taskCard/index.css'
 function App() {
-  const task = {
-    title: 'Sample Task',
-    description: 'This is a sample task description.',
-    completed: false
-  }
+  const tasks = [
+    {
+      title: 'Sample Task',
+      description: 'This is a sample task description.',
+      completed: false
+    },
+    {
+      title: 'Another Task',
+      description: 'This is another task description.',
+      completed: true
+    },
+    {
+      title: 'Third Task',
+      description: 'This is the third task description.',
+      completed: false
+    }
+  ]
   return (
     <>
-      <TaskCard task={task} />
+      {tasks.map((task, index) => (
+        <TaskCard key={index} task={task} />
+      ))}
     </>
   )
 }
